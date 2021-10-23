c59cc68287c4c2b0c16df6147fa40cecc62dc241
Junaid <zackthunder747@gmail.com>
2021-10-23 18:53:40 +0530

    Fixed eventListener fallthrough, changed navbar expanding by click and not hover

--- magic.js
+++ magic.js
@@ -1,18 +1,65 @@
-/* I feel like these are pretty self explanatory functions */
+/* -----Variables----- */
+/* Story elements */
+const ch1 = document.getElementById("ch1");
+const ch2 = document.getElementById("ch2");
+const ch3 = document.getElementById("ch3");
+const chapters = [ch1, ch2, ch3];
+
+/* Chapter header */
+const head = document.getElementById("chapterHead");
+
+/* Navbar elements */
+const navbar = document.getElementById("sidebar");
+
+/* Button elements */
+const ch1Btn = document.getElementById("ch1-btn");
+const ch2Btn = document.getElementById("ch2-btn");
+const ch3Btn = document.getElementById("ch3-btn");
+const dark = document.getElementById("darkMode-btn");
+
+/* -----Event Listeners----- */
+/* Page Load events */
+document.getElementById("body").addEventListener("load", header(1)); 
+document.getElementById("body").addEventListener("load", blink(ch1));
+
+/* Expand navbar */
+/* The toggle class function is awesome, btw. 
+*  TODO: Use toggle for dark mode! 
+*/
+document.getElementById("navExpand").addEventListener("click", function(){ navbar.classList.toggle("sidebarExtended"); });
+
+/* "Load" chapters */
+/* Apparently, when you have parameters involved, wrapping it in an anonymous function is good 
+*  It also seems to have fixed the fallthrough issue */
+ch1Btn.addEventListener("click", function() { blink(ch1); });
+ch2Btn.addEventListener("click", function() { blink(ch2); });
+ch3Btn.addEventListener("click", function() { blink(ch3); });
+
+/* -----Functions-----*/
+/* Show */
 function eyeOpen (divID)
 { divID.style.display = "block"; }
-
+/* Hide */
 function eyeClose (divID) 
 { divID.style.display = "none"; }
 
 /* Iterates through chapters, hides unselected, displays selected */
 function blink (divID)
 {
+
+    console.log([divID]);
+
     chapters.forEach(blinkInside);
 
-    /* Apparently this is a closure! Or... has closures? */
-    /* Closures are things that can access outer function's scope from an inner function */
-    /* blinkInside uses the divID from the outer function! */
+    /*  Apparently this is a closure! Or... has closures? */
+    /*  Closures are things that can access outer function's scope from an inner function */
+    /*  blinkInside uses the divID from the outer function! */
+    
+    /*  The function iterates through the array of chapters 
+    *   divID here is the divID of the chapter that was selected from navbar
+    *   if divID is not the same, that chapter is hidden
+    *   If divID matches the one selected from navbar, it is displayed
+    */
     function blinkInside(item, index)
     {
         if(!(item === divID))
@@ -38,23 +85,3 @@ function header(num)
         break;
     }
 }
-
-/* Getting the elements into variables for easier access */
-/* Story elements */
-const ch1 = document.getElementById("ch1");
-const ch2 = document.getElementById("ch2");
-const ch3 = document.getElementById("ch3");
-const chapters = [ch1, ch2, ch3];
-
-const head = document.getElementById("chapterHead");
-
-document.getElementById("body").addEventListener("load", header(1));
-document.getElementById("body").addEventListener("load", blink(ch1));
-
-/* Something is causing the event handlers to fall through */
-/* Figure it out to remove onclick on index.html */
-/*document.getElementById("ch1btn").addEventListener("click", blink(ch1));
-
-document.getElementById("ch2btn").addEventListener("click", blink(ch2));
-
-document.getElementById("ch3btn").addEventListener("click", blink(ch3));*/
